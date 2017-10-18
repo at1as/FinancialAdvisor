d5b092392f08a4e0fd1aa14db5705635249dd66d
Jason Willems <jason@willems.ca>
2017-10-18 00:18:02 -0700

    Minor improvements

--- purchase.rb
+++ purchase.rb
@@ -1,12 +1,13 @@
-require 'byebug'
+class FinancialAdvisor
 
-class Purchase
+  attr_reader :valid_purchases
 
-  attr_reader :valid_combinations
-
-  def initialize(budget)
+  def initialize(budget, item_type = "items")
     @budget = budget.to_f
-    @valid_combinations = []
+    @item_type = item_type
+    @valid_purchases = []
+
+    compute_combinations
   end
 
 
@@ -18,12 +19,13 @@ class Purchase
       "The Bed of Procrustes" => 11.99,
       "The Innovators" => 13.99,
       "Play Bigger: How Pirates, Dreamers, and Innovators Create and Dominate Markets" => 16.00,
-      "After On" => 13.99,
       "Homo Deus" => 17.99,
       "A Universe From Nothing" => 11.99,
       "A Piece of the Sun" => 10.99,
       "An Extraordinary Time: The End of the Postwar Boom and the Return to the Ordinary Economy" => 18.99,
-      "The Inevitable" => 13.99
+      "The Inevitable" => 13.99,
+      "End of the Asian Century" => 15.00,
+      "The Next America: Boomers, Millennials and the Looming Generational Slowdown" => 11.99
     }
   end
 
@@ -39,13 +41,13 @@ class Purchase
 
 
   def combination(items, remaining_items)
-    # TODO: There's a better recursive solution that doesn't need to continually append to @valid_combinations instance variable
+    # TODO: There's a better recursive solution that doesn't need to continually append to @valid_purchases instance variable
     current_total = item_sum(items)
     
     remaining_items.each do |next_item|
       if item_sum(items + [next_item]) > @budget
-        @valid_combinations << items
-        @valid_combinations.uniq!
+        @valid_purchases << items.sort
+        @valid_purchases.uniq!
       else
         combination(
           items + [next_item],
@@ -57,42 +59,49 @@ class Purchase
 
 
   def compute_combinations
+    @valid_purchases = []
+
     item_tuples.map do |item|
       combination([item], item_tuples.reject {|x| x == item})
     end
   end
 
 
-  def best_option_by_cost(results = 10)
-    @valid_combinations.sort_by { |item_list| item_sum(item_list) }.reverse[0..results]
+  def best_options_by_cost(results = 10)
+    @valid_purchases.sort_by { |item_list| item_sum(item_list) }.reverse[0..results]
   end
 
 
-  def best_option_by_number_of_items(results = 10)
-    @valid_combinations.sort_by { |item_list| item_list.length }.reverse[0..results]
+  def best_options_by_number_of_items(results = 10)
+    @valid_purchases.sort_by { |item_list| item_list.length }.reverse[0..results]
   end
 
 
-  def pretty_print(results = 10, sort_by = :price)
-    best_options = (sort_by == :price ? best_option_by_cost(results) : best_option_by_number_of_items(results))
+  def print_best_options(results = 10, sort_by = :price)
+    best_options = (sort_by == :price ? best_options_by_cost(results) : best_options_by_number_of_items(results))
 
     best_options.each do |result|
-      total_price = item_sum(result)
-      items = result.map { |name, price| "\"#{name}\" (#{price})" }.join(", ")
+      total_price = sprintf('%.2f', item_sum(result))
+      items       = result.map { |name, price| "\"#{name}\" ($#{price})" }.join(", ")
 
-      puts "#{total_price} : #{items}"
+      puts "$#{total_price} : #{result.length} #{@item_type}   ==>   #{items}"
     end
   end
 
 end
 
 
-purchases = Purchase.new(30.00)
-purchases.compute_combinations
+budget = 30.00
 
-puts "\nBest options by price\n"
-purchases.pretty_print(50, :price)
+puts "\nBudget: $#{budget}"
 
-puts "\nBest options by price\n"
-purchases.pretty_print(50, :cost)
+potential_purchases = FinancialAdvisor.new(budget, "books")
+
+puts "\n\n#### Best Options by Price:\n\n"
+potential_purchases.print_best_options(8, :price)
+
+puts "\n\n#### Best Options by Number of Books Purchased:\n\n"
+potential_purchases.print_best_options(8, :cost)
+
+puts ?\n
 
